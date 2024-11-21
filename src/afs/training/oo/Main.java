f94f94d6e5d9f7821e34504dad7fcb5dee7d4eac
SHENZA <shenza@oocl.com>
2024-11-21 11:22:36 +0800

    [OO]feat: AC2

--- src/afs/training/oo/Main.java
+++ src/afs/training/oo/Main.java
@@ -3,5 +3,6 @@ package afs.training.oo;
 public class Main {
     public static void main(String[] args) {
         new Bus("Cool Bus", 25).speedUp();
+        new Truck("Big Truck", 20).speedUp();
     }
 }
